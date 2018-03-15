c4aafeb76afebfb833e8df4305ead47c81893e54
Jim Pivarski <jpivarski@gmail.com>
2018-03-15 17:26:56 -0500

    save constructor

--- pandhist.py
+++ pandhist.py
@@ -47,61 +47,84 @@ def _binintervals(numbins, low, high, closed):
     return _irrbinintervals(edges, closed)
 
 def bin(numbins, low, high, expression, closed="left"):
-    return HistogramLayoutBinning([_binintervals(numbins, low, high, closed)], [expression])
+    return HistogramLayoutBinning([("bin", numbins, low, high, expression, closed)],
+                                  [_binintervals(numbins, low, high, closed)],
+                                  [expression])
 
 def irrbin(edges, expression, closed="left"):
-    return HistogramLayoutBinning([_irrbinintervals(edges, closed)], [expression])
+    return HistogramLayoutBinning([("irrbin", edges, expression, closed)],
+                                  [_irrbinintervals(edges, closed)],
+                                  [expression])
 
 def frac(expression):
-    return HistogramLayoutBinning([["numer", "denom"]], [expression])
+    return HistogramLayoutBinning([("frac", expression)],
+                                  [["numer", "denom"]],
+                                  [expression])
 
 def cut(expression):
-    return HistogramLayoutBinning([["pass", "fail"]], [expression])
+    return HistogramLayoutBinning([("cut", expression)],
+                                  [["pass", "fail"]],
+                                  [expression])
 
 class HistogramLayout(object):
-    def __init__(self, indexes, expressions, columns):
+    def __init__(self, specification, indexes, expressions, columns):
+        self.specification = specification
         self.indexes = indexes
         self.expressions = expressions
         self.columns = columns
 
-    def _newcolumn(self, column):
+    def _newcolumn(self, specification, column):
         if column in self.columns:
             raise ValueError("attempting to attach {0} twice".format(repr(column)))
-        return HistogramLayout(self.indexes, self.expressions, self.columns + [column])
+        return HistogramLayout(specification, self.indexes, self.expressions, self.columns + [column])
 
     def sumw2(self):
-        return self._newcolumn("sumw2")
+        return self._newcolumn(self.specification + [("sumw2",)], "sumw2")
 
     def sumwx(self, expression):
-        return self._newcolumn("sumwx({0})".format(expression))
+        return self._newcolumn(self.specification + [("sumwx", expression)], "sumwx({0})".format(expression))
 
     def sumwx2(self, expression):
-        return self._newcolumn("sumwx2({0})".format(expression))
+        return self._newcolumn(self.specification + [("sumwx2", expression)], "sumwx2({0})".format(expression))
 
     def min(self, expression):
-        return self._newcolumn("min({0})".format(expression))
+        return self._newcolumn(self.specification + [("min", expression)], "min({0})".format(expression))
 
     def max(self, expression):
-        return self._newcolumn("max({0})".format(expression))
+        return self._newcolumn(self.specification + [("max", expression)], "max({0})".format(expression))
 
     def toDF(self):
-        return pandas.DataFrame(
+        out = pandas.DataFrame(
             index=pandas.MultiIndex.from_product(self.indexes, names=self.expressions),
             columns=self.columns,
             data=dict((x, float("inf") if x.startswith("min") else float("-inf") if x.startswith("max") else 0.0) for x in self.columns))
+        out.specification = specification
+        out.__class__ = Histogram
+        return out
 
 class HistogramLayoutBinning(HistogramLayout):
-    def __init__(self, indexes, expressions):
-        super(HistogramLayoutBinning, self).__init__(indexes, expressions, ["sumw"])
+    def __init__(self, specification, indexes, expressions):
+        super(HistogramLayoutBinning, self).__init__(specification, indexes, expressions, ["sumw"])
 
     def bin(self, numbins, low, high, expression, closed="left"):
-        return HistogramLayoutBinning(self.indexes + [_binintervals(numbins, low, high, closed)], self.expressions + [expression])
+        return HistogramLayoutBinning(self.specification + [("bin", numbins, low, high, expression, closed)],
+                                      self.indexes + [_binintervals(numbins, low, high, closed)],
+                                      self.expressions + [expression])
 
     def irrbin(self, edges, expression, closed="left"):
-        return HistogramLayoutBinning(self.indexes + [_irrbinintervals(edges, closed)], self.expressions + [expression])
+        return HistogramLayoutBinning(self.specification + [("irrbin", edges, expression, closed)],
+                                      self.indexes + [_irrbinintervals(edges, closed)],
+                                      self.expressions + [expression])
 
     def frac(self, expression):
-        return HistogramLayoutBinning(self.indexes + [["numer", "denom"]], self.expressions + [expression])
+        return HistogramLayoutBinning(self.specification + [("frac", expression)],
+                                      self.indexes + [["numer", "denom"]],
+                                      self.expressions + [expression])
 
     def cut(self, expression):
-        return HistogramLayoutBinning(self.indexes + [["pass", "fail"]], self.expressions + [expression])
+        return HistogramLayoutBinning(self.specification + [("cut", expression)],
+                                      self.indexes + [["pass", "fail"]],
+                                      self.expressions + [expression])
+
+class Histogram(pandas.DataFrame):
+    pass
